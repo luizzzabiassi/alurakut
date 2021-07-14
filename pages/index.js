40e7e5dd11d28c055681326b8e7c9a479b9226d9
Luiza Biassi <luiza.biassi@gmail.com>
2021-07-14 15:02:17 -0300

    seguidores e datocms

--- pages/index.js
+++ pages/index.js
@@ -22,6 +22,29 @@ function ProfileSidebar(propriedades){
   )
 }
 
+function ProfileRelationsBox(propriedades){
+  return(
+    <ProfileRelationsBoxWrapper>
+      <h2 className='smallTitle'>
+        {propriedades.title} ({propriedades.items.length})
+      </h2>
+
+      <ul>
+        {/*seguidores.map((itemAtual) => {
+          return(
+            <li key={itemAtual}>
+              <a href={`https://github.com/${itemAtual}.png`}>
+                <img src={itemAtual.image} />
+                <span>{itemAtual.title}</span>
+              </a>
+            </li>
+          )
+        })*/}
+      </ul>
+    </ProfileRelationsBoxWrapper>
+  )
+}
+
 export default function Home(){
   const githubUser = 'luizzzabiassi';
 
@@ -44,6 +67,18 @@ export default function Home(){
     'marcobrunodev', 
     'felipefialho'
   ]
+
+  const [seguidores, setSeguidores] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch(`https://api.github.com/users/luizzzabiassi/followers`)
+    .then((respServidor) => {
+      return respServidor.json();
+    })
+    .then((respCompleta) => {
+      setSeguidores(respCompleta);
+    })
+  }, [])
 
   return(
     <>
@@ -89,6 +124,7 @@ export default function Home(){
           </Box>
         </div>
         <div className='profileRelationsArea' style={{gridArea: 'profileRelationsArea'}}>
+          <ProfileRelationsBox title='Seguidores' items={seguidores} />
           <ProfileRelationsBoxWrapper>
             <h2 className='smallTitle'>
               Comunidades ({comunidades.length})
